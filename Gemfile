c3be18dc3b2151159c3e345505977e194f8873e4
sleepingkingstudios <merlin@sleepingkingstudios.com>
2014-10-13 15:34:11 -0400

    Add BDD using RSpec.

--- Gemfile
+++ Gemfile
@@ -6,10 +6,10 @@ gem 'rails', '4.1.6'
 gem 'sqlite3'
 
 ### Assets ###
-gem 'sass-rails', '~> 4.0.3'
+gem 'sass-rails',   '~> 4.0.3'
 gem 'coffee-rails', '~> 4.0.0'
-gem 'jquery-rails'
-gem 'uglifier', '>= 1.3.0' # Compressor for JavaScript assets
+gem 'jquery-rails', '~> 3.1.2'
+gem 'uglifier',     '>= 1.3.0' # Compressor for JavaScript assets
 
 ### Support ###
 gem 'jbuilder', '~> 2.0' # Build JSON APIs. Read more: https://github.com/rails/jbuilder
@@ -19,5 +19,12 @@ gem 'spring', :group => :development
 
 ### Testing ###
 group :development, :test do
+  gem 'rspec-rails',                 '~> 3.1'
+  gem 'rspec-collection_matchers',   '~> 1.0.0'
+  gem 'rspec-sleeping_king_studios', '>= 2.0.0.beta.0'
 
+  gem 'factory_girl_rails', '~> 4.4.1'
+  gem 'database_cleaner',   '~> 1.3.0'
+
+  gem 'pry', '~> 0.10.1'
 end # group
---
+++ spec/rails_helper.rb
@@ -0,0 +1,35 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require 'spec_helper'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+# Add additional requires below this line. Rails is not loaded until this point!
+
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+# Checks for pending migrations before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  # Automatically mix in different behaviours to your tests based on their file
+  # location, for example enabling you to call `get` and `post` in specs under
+  # `spec/controllers`.
+  #
+  # The different available types are documented in the features, such as in
+  # https://relishapp.com/rspec/rspec-rails/docs
+  config.infer_spec_type_from_file_location!
+
+  config.before(:suite) do
+    DatabaseCleaner[:active_record].strategy = :transaction
+    DatabaseCleaner[:active_record].clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
